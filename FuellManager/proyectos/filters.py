5aa58916a317e9d8294c04ddada64e1fefae4b54
Patricio <patricioajv@gmail.com>
2024-07-07 22:11:37 -0400

    se aplican correcciones y se ajustan los filtros añadiendo funciones

--- FuellManager/proyectos/filters.py
+++ FuellManager/proyectos/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Sum
 from proyectos.models import RegistroProduccion
 
 class ProduccionFilter(django_filters.FilterSet):
@@ -6,7 +7,12 @@ class ProduccionFilter(django_filters.FilterSet):
     nombre_planta = django_filters.CharFilter(field_name='codigo_combustible__planta__nombre', label='Nombre de la Planta')
     ano_produccion = django_filters.NumberFilter(field_name='fecha_produccion__year', label='Año de produccion')
     mes_produccion = django_filters.NumberFilter(field_name='fecha_produccion__month', label='Mes de Produccion')
+    total_producido = django_filters.Filter(method='filter_total_producido', label='Total Producido')
+
 
     class Meta:
         model = RegistroProduccion
-        fields = ['nombre_combustible', 'nombre_planta', 'ano_produccion', 'mes_produccion']
+        fields = ['nombre_combustible', 'nombre_planta', 'ano_produccion', 'mes_produccion', 'total_producido']
+
+    def filter_total_producido(self, queryset, name, value):
+        return queryset.annotate(total_producido=Sum('litros_producidos'))
--- FuellManager/rest/views.py
+++ FuellManager/rest/views.py
@@ -5,7 +5,8 @@ from .serializers import ProduccionSerializer, PlantaSerializer, ProductoSeriali
 from proyectos.models import Planta, Producto, RegistroProduccion
 from django_filters.rest_framework import DjangoFilterBackend
 from proyectos.filters import ProduccionFilter
-
+from django.db.models import Sum
+from rest_framework.response import Response
 
 class PlantaViewSet(viewsets.ModelViewSet):
     queryset = Planta.objects.all()
@@ -22,3 +23,16 @@ class ProduccionViewSet(viewsets.ModelViewSet):
 
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProduccionFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+
+        # Calcular la suma de litros producidos para los registros filtrados
+        total_producido = queryset.aggregate(Sum('litros_producidos'))['litros_producidos__sum']
+
+        # Agregar el total_producido al contexto de respuesta
+        data = serializer.data
+        data.append({'total_producido': total_producido})
+
+        return Response(data)
